231295ebbfa8cbdb1cffa27aef445b5530123c8a
Jamal U Din <jamaluddin14926@gmail.com>
2023-07-21 10:34:40 +0500

    Componenets folder is created inside src folder inside weatherApp componenets will be placed. Search component is created for search bar where user can search weather of cities. API from RapidApi is used to search cities. api.js file contains API for cities.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
+import Search from './components/search/Search';
 
 function App() {
   return (
-    <>
-    <div className="heading">
-      <h1>Let's See How Nature is Feelin</h1>
+    <div className='container'>
+    <Search/>
     </div>
-    </>
   );
 }
 
---
+++ src/api.js
@@ -0,0 +1,8 @@
+export const geoApiOptions = {
+    method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2328910249mshccaf7de7f44f125p1d1930jsnac1c9b82fc42',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
